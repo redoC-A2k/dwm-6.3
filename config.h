9ea22a114bdc15507d108affe24c5af0d3e0cac4
redoC-A2k <afshanahmeda2k@gmail.com>
2022-04-08 01:08:59 +0530

    added restartsig patch

--- config.h
+++ config.h
@@ -104,6 +104,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
   {0,                             XF86XK_AudioMute,spawn,    {.v = mutecmd}},
   {0,                             XF86XK_AudioRaiseVolume,spawn,    {.v = volupcmd}},
   {0,                             XF86XK_AudioLowerVolume,spawn,    {.v = voldowncmd}},
